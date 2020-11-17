b9fec2b1e240d74e2a20e27fab69d806294778de
Jchyder <74035198+Jchyder@users.noreply.github.com>
2020-11-17 00:55:16 -0600

    Update Item.java
    HI

--- Item.java
+++ Item.java
@@ -1,3 +1,54 @@
 public class Item {
     
+    public void FlameSwordofSadness() //Legendary Item
+    {
+        
+
+    }
+
+    public void TheComicallyLargeSpoon() //Legendary Item
+    {
+
+
+    }
+
+    public void EnergySword()
+    {
+
+
+    }
+
+    public void ACTOFVIOLENCE() //Legendary
+    {
+
+
+
+    }
+
+    public void LightningFishingRod() //Common Item
+    {
+
+
+
+    }
+
+    public void FishSword()
+    {
+
+
+
+    }
+
+    public void SlimCythe()
+    {
+
+
+
+
+    }
+
+
+
+
+
 }
