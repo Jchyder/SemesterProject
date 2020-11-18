81dfa71b910fc6ef402ad8c7253826a1bba1b228
Jchyder <74035198+Jchyder@users.noreply.github.com>
2020-11-17 22:14:55 -0600

    New Item and Generator update
    Hi

--- Generator.java
+++ Generator.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class Generator 
 {
     static Random rand = new Random();
+    static Item item = new Item();
 
     public static int randomBetween(int num1, int num2)
     {
@@ -13,14 +14,63 @@ public class Generator
     public static void randomLoot() {
         int randomInt = rand.nextInt(100);
 
-        //Flame Sword
         if(randomInt == 0)
         {
-            text.addItem("Flame Sword", 5, "Dexterity", 1, 0);
+        item.FlameSwordofSadness();
         }
-        else
+        
+        // -Legendary
+        if(randomInt >= 0 & randomInt <= 4)
         {
-            System.out.println("yeet");
+        item.TheComicallyLargeSpoon();
+        }
+
+        //-Legenday
+        if(randomInt >= 5 & randomInt <= 10)
+        {
+        item.EnergySword();
+        }
+
+        //-Legenday
+        if(randomInt >= 11 & randomInt <= 15)
+        {
+        item.ACTOFVIOLENCE();
+        }
+
+        //-Legenday
+        if(randomInt >= 16 & randomInt <= 20)
+        {
+        item.RubberChicken();
+        }
+
+        //-Legenday
+        if(randomInt >= 16 & randomInt <= 20)
+        {
+        item.HashSlingingSlashSingingSpatula();
+        }
+
+        //-Legenday
+        if(randomInt >= 21 & randomInt <= 25)
+        {
+        item.UnnaturallyBigNerfSword();
+        }
+
+        //-Common
+        if(randomInt >= 26 & randomInt <= 50)
+        {
+        item.GrindersPickAxe();
+        }
+
+        //-Common
+        if(randomInt >= 51 & randomInt <= 75)
+        {
+        item.FishSword();
+        }
+
+        //-Common
+        if(randomInt >= 76 & randomInt <= 100)
+        {
+        item.LightningFishingRod();
         }
     }
 }
--- Item.java
+++ Item.java
@@ -1,54 +1,68 @@
 public class Item {
-    
-    public void FlameSwordofSadness() //Legendary Item
-    {
-        
+    int WeaponDamage;
 
-    }
 
+    //Rare Weapons
     public void TheComicallyLargeSpoon() //Legendary Item
     {
-
-
-    }
-
-    public void EnergySword()
-    {
-
+        WeaponDamage = 100;
 
     }
 
-    public void ACTOFVIOLENCE() //Legendary
+    public void FlameSwordofSadness() //Legendary Item
     {
+        WeaponDamage = 30;
 
+    }
 
+    public void EnergySword() //Legendary items
+    {
+        WeaponDamage = 45;
+
+    }
+
+    public void ACTOFVIOLENCE() //Legendary items
+    {
+        WeaponDamage = 90;
+
+    }
+
+    public void RubberChicken() //Legendary items
+    {
+        WeaponDamage = 81;
+
+    }
+
+    public void HashSlingingSlashSingingSpatula() //Legendary items
+    {
+        WeaponDamage = 78;
+
+    }
+
+    public void UnnaturallyBigNerfSword() //Legendary items
+    {
+        WeaponDamage = 52;
+
+    }
+
+    //All commone weapons
+
+    public void GrindersPickAxe() //Common item
+    {
+        WeaponDamage = 15;
+
+    }
+
+    public void FishSword() //common item
+    {
+        WeaponDamage = 8;
 
     }
 
     public void LightningFishingRod() //Common Item
     {
-
-
-
-    }
-
-    public void FishSword()
-    {
-
-
+        WeaponDamage = 12;
 
     }
-
-    public void SlimCythe()
-    {
-
-
-
-
-    }
-
-
-
-
 
 }
